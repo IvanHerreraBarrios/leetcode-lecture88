d815d3886a6abcbb4deaa4ecc1c30e5aae574524
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-11-21 14:40:12 -0600

    leetcode-lecture88

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ var isRectangleOverlap = function(rec1, rec2) {
     if((rec1[2] > rec2[0] && rec1[3] > rec2[1]) && (rec1[0] < rec2[2] && rec1[1] < rec2[3])) return true
     return false
 };
+
+//solution 1
